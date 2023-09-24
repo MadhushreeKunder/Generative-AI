2435434fcd827e7086d9d19fc4eb0d6edc163b6f
Madhushree Kunder <madhushreekunder@gmail.com>
2023-09-24 19:43:55 +0530

    change data

--- video-summariser/src/Video/VideoInput.jsx
+++ video-summariser/src/Video/VideoInput.jsx
@@ -5,13 +5,14 @@ import { MdContentCopy } from "react-icons/md";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+const data =
+  "An apple is a round, edible fruit produced by an apple tree (Malus domestica). Apple trees are cultivated worldwide and are the most widely grown species in the genus Malus. The tree originated in Central Asia, where its wild ancestor, Malus sieversii, is still found. Apples have been grown for thousands of years in Asia and Europe and were introduced to North America by European colonists. Apples have religious and mythological significance in many cultures, including Norse, Greek, and European Christian tradition.Apples grown from seed tend to be very different from those of their parents, and the resultant fruit frequently lacks desired characteristics. Generally, apple cultivars are propagated by clonal grafting onto rootstocks. Apple trees grown without rootstocks tend to be larger and much slower to fruit after planting. Rootstocks are used to control the speed of growth and the size of the resulting tree, allowing for easier harvesting.There are more than 7,500 cultivars of apples. Different cultivars are bred for various tastes and uses, including cooking, eating raw, and cider production. Trees and fruit are prone to fungal, bacterial, and pest problems, which can be controlled by a number of organic and non-organic means. In 2010, the fruit's genome was sequenced as part of research on disease control and selective breeding in apple production.";
+
 export const VideoInput = () => {
   const [summary, setSummary] = useState("");
 
   useEffect(() => {
-    setSummary(
-      "Duis deserunt qui dolore exercitation id aute excepteur laborum consequat culpa esse elit exercitation cupidatat. Occaecat id ametdeserunt tempor reprehenderit. Aliqua aliqua quis amet nisi ullamcoeu magna. Excepteur fugiat occaecat ipsum eu qui esse nisi fugiatconsequat. Magna quis dolore anim ad est non officia proident essead. Voluptate irure fugiat elit do anim eu commodo veniam mollit.Velit irure exercitation duis quis sit irure. Duis deserunt quidolore exercitation id aute excepteur laborum consequat culpa esseelit exercitation cupidatat. Occaecat id amet deserunt temporreprehenderit. Aliqua aliqua quis amet nisi ullamco eu magna.Excepteur fugiat occaecat ipsum eu qui esse nisi fugiat consequat.Magna quis dolore anim ad est non officia proident esse ad.Voluptate irure fugiat elit do anim eu commodo veniam mollit. Velitirure exercitation duis quis sit irure. Duis deserunt qui doloreexercitation id aute excepteur laborum consequat culpa esse elitexercitation cupidatat. Occaecat id amet deserunt temporreprehenderit. Aliqua aliqua quis amet nisi ullamco eu magna.Excepteur fugiat occaecat ipsum eu qui esse nisi fugiat consequat.Magna quis dolore anim ad est non officia proident esse ad.Voluptate irure fugiat elit do anim eu commodo veniam mollit. Velitirure exercitation duis quis sit irure. Duis deserunt qui doloreexercitation id aute excepteur laborum consequat culpa esse elitexercitation cupidatat. Occaecat id amet deserunt temporreprehenderit. Aliqua aliqua quis amet nisi ullamco eu magna.Excepteur fugiat occaecat ipsum eu qui esse nisi fugiat consequat.Magna quis dolore anim ad est non officia proident esse ad.Voluptate irure fugiat elit do anim eu commodo veniam mollit. Velitirure exercitation duis quis sit irure.",
-    );
+    setSummary(data);
   }, []);
 
   function handleDownload(summary) {
